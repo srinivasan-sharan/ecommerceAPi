6bd9a96042f7122167c4b4bc8a44c4017e0513e0
srinivasan-sharan <shrnsrnvsn14@gmail.com>
2023-08-23 14:39:04 +0530

    initial project

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+    console.log(`ndoe is running on ${port}`);
+})
